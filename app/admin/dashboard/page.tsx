8ae0b3064b6bad4c4798f2fb1d3ac5cc9d3c0108
sickmanmazen <m.chaouch007@gmail.com>
2024-02-22 20:28:52 +0100

    no admin

--- app/admin/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageDashboard = () => {
-  return <div>PageDashboard</div>;
-};
-
-export default PageDashboard;
--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AdminLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="w-full h-screen bg-stone-800 text-stone-100">
-      {children}
-    </div>
-  );
-};
-
-export default AdminLayout;
